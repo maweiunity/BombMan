7540079e1acd6c36bc3e88bd961227a2bab591b8
MaWei <1123265518@qq.com>
2022-10-26 22:32:24 +0800

    env prefabs

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    Rigidbody2D playerRb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void playerMove()
+    {
+        if (Input.GetButtonDown(""))
+        {
+            
+        }
+    }
+}
