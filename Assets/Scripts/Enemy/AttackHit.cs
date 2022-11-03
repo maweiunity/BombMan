5ee6151c155472b2a9dc5830a2fc04b054c45698
MaWei <1123265518@qq.com>
2022-11-03 18:43:55 +0800

    BaldPirate Attack

--- Assets/Scripts/Enemy/AttackHit.cs
+++ Assets/Scripts/Enemy/AttackHit.cs
@@ -2,13 +2,56 @@ using UnityEngine;
 
 public class AttackHit : MonoBehaviour
 {
+    public bool IsDiaup = false;
+
     // 普通攻击
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        switch (other.tag)
         {
-            // 攻击到了，减伤
-            other.GetComponent<IHurt>().HitHurt(gameObject.GetComponentInParent<Enemy>().AK);
+            case "Player":
+                attackPlayer(other);
+                break;
+            case "Bomb":
+                attackBomb(other);
+                break;
+        }
+    }
+
+    // 攻击玩家
+    void attackPlayer(Collider2D other)
+    {
+        // 是否会击飞
+        diaup(other);
+        // 攻击到了，减伤
+        other.GetComponent<IHurt>().HitHurt(gameObject.GetComponentInParent<Enemy>().AK);
+    }
+
+    // 攻击炸弹
+    void attackBomb(Collider2D other)
+    {
+        diaup(other);
+    }
+
+    // 确定左右方向
+    int getDiration(Collider2D other)
+    {
+        if (transform.position.x > other.transform.position.x)
+        {
+            return -1;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+
+    // 击飞
+    void diaup(Collider2D other)
+    {
+        if (IsDiaup)
+        {
+            other.GetComponent<Rigidbody2D>().AddForce(new Vector2(getDiration(other), 1) * gameObject.GetComponentInParent<Enemy>().AK, ForceMode2D.Impulse);
         }
     }
 }
